69dbded086e79dc71a3c95c08bbf701ef905c4ea
Pruthil Tejani <pruthiltejani.work@gmail.com>
2025-11-08 16:56:18 +0530

    Add order management functionality with order placement and status update features; implement admin middleware for access control.

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const authMiddleware = require("../middlewares/authMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 const {
   createFoodController,
   getAllFoodsController,
@@ -8,6 +9,8 @@ const {
   getFoodByResturantController,
   updateFoodController,
   deleteFoodController,
+  placeOrderController,
+  orderStatusController,
 } = require("../controllers/foodController");
 
 const router = express.Router();
@@ -30,5 +33,16 @@ router.put("/update/:id", authMiddleware, updateFoodController);
 
 // DELETE FOOD
 router.delete("/delete/:id", authMiddleware, deleteFoodController);
+
+// PLACE ORDER
+router.post("/placeorder", authMiddleware, placeOrderController);
+
+// ORDER STATUS
+router.post(
+  "/orderStatus/:id",
+  authMiddleware,
+  adminMiddleware,
+  orderStatusController
+);
 
 module.exports = router;
